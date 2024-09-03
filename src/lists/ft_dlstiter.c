030914645b74b72434af7cc45d5f77aa1f37547d
xenia-0101 <xeniavislocka@gmail.com>
2024-09-04 00:02:00 +0200

    4.9.2024 add iteration by value

--- src/lists/ft_dlstiter.c
+++ src/lists/ft_dlstiter.c
@@ -6,7 +6,7 @@
 /*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 10:07:00 by xenia             #+#    #+#             */
-/*   Updated: 2024/09/01 13:10:05 by xenia            ###   ########.fr       */
+/*   Updated: 2024/09/03 23:35:46 by xenia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,18 @@ void	ft_dlstiter_i(t_dlist *lst, size_t i, void (*f)(t_dlist *))
 		return ;
 	}
 	if (lst->next)
-		ft_dlstiter_i(lst, i, f);
+		ft_dlstiter_i(lst->next, i, f);
 	write(1, "No such index found\n", 20);
+}
+
+size_t	ft_dlstiter_v(t_dlist *lst, size_t v)
+{
+	if (!lst)
+		return (-1);
+	if (lst->value == v)
+	{
+		return (lst->index);
+	}
+	if (lst->next)
+		ft_dlstiter_v(lst->next, v);
 }
